c5998fbeb2a71fcb60e265a1f1715b616758eddb
Sol Lo <sol@solazy.me>
2026-03-18 11:13:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,10 @@ echo "Output Dir: ${INPUT_DIR}"
 # 创建输出目录
 mkdir -p "${INPUT_DIR}"
 
-# 运行 Python 脚本（从环境变量读取 Cookie）
+# 切换到我们放置代码的目录，或者直接设置 PYTHONPATH
+export PYTHONPATH="/action_app"
+
+# 运行 Python 脚本
 python -m src.douban_client \
     "${INPUT_ID}" \
     "${INPUT_DIR}" \
